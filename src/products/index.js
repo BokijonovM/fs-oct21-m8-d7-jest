151cee87103211a1e69accc43e061d1830af588d
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-03-22 16:14:59 +0100

    updated

--- src/products/index.js
+++ src/products/index.js
@@ -1,20 +1,47 @@
-import express from "express"
-import Product from "./model.js"
+import express from "express";
+import Product from "./model.js";
 const productsRouter = express.Router();
 
 productsRouter
-    .get('/', async (req, res) => {
-        const products = await Product.find({});
-        res.status(200).send(products);
-    })
-    .post('/', async (req, res) => {
-        try {
-            const product = new Product(req.body)
-            await product.save()
-            res.status(201).send(product)
-        } catch (error) {
-            res.status(400).send(error)
-        }
-    })
+  .get("/", async (req, res) => {
+    const products = await Product.find({});
+    res.status(200).send(products);
+  })
+  .get("/:id", async (req, res, next) => {
+    try {
+      const proId = req.params.id;
 
-export default productsRouter
+      const product = await Product.findById(proId);
+      if (product) {
+        res.send(product);
+      } else {
+        res.status(404).send(`Product with id ${proId} not found!`);
+      }
+    } catch (error) {
+      next(error);
+    }
+  })
+  .post("/", async (req, res) => {
+    try {
+      const product = new Product(req.body);
+      await product.save();
+      res.status(201).send(product);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  })
+  .delete("/:id", async (req, res, next) => {
+    try {
+      const accId = req.params.id;
+      const deletedAcc = await Product.findByIdAndDelete(accId);
+      if (deletedAcc) {
+        res.status(204).send(`Product with id ${accId} deleted!`);
+      } else {
+        res.status(404).send(`Product with id ${accId} not found!`);
+      }
+    } catch (error) {
+      next(error);
+    }
+  });
+
+export default productsRouter;
